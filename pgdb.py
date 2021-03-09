fd6b00a833e1b9045945f04a59d86b6915328960
Eddie <eddie.lazar@yahoo.com>
2021-03-09 01:21:37 -0500

    add game creation to last_move for sorting

--- pgdb.py
+++ pgdb.py
@@ -14,14 +14,14 @@ cursor = conn.cursor()
 
 sql = {
     "getCompletedGames": "SELECT * FROM chess.games where completed=true AND (white_player=%s OR black_player=%s)",
-    "getActiveGames": "SELECT * FROM chess.games where completed=false AND (white_player=%s OR black_player=%s)",
+    "getActiveGames": "SELECT * FROM chess.games where completed=false AND (white_player=%s OR black_player=%s) ORDER BY last_move DESC",  
     "getUser": "SELECT * FROM chess.users WHERE name=%s",
     "getGame": "SELECT * FROM chess.games WHERE id=%s",
     "checkLogin": "SELECT * FROM chess.users WHERE name=%s AND password_hash=%s",
     
     "createUser": "INSERT INTO chess.users (name, password_hash, email, id) VALUES (%s, %s, %s, %s)",
     "createStat": "INSERT INTO chess.stats (userid) VALUES (%s)",
-    "createGame": "INSERT INTO chess.games (id, white_player, black_player, boardstate, completed, time_started) VALUES (%s, %s, %s, %s, %s, %s)",
+    "createGame": "INSERT INTO chess.games (id, white_player, black_player, boardstate, completed, time_started, last_move) VALUES (%s, %s, %s, %s, %s, %s, %s)",
     
     "updateBoardstate": "UPDATE chess.games SET boardstate=%s, last_move=%s WHERE id=%s",
     "endGame": "UPDATE chess.games SET time_ended=%s, completed=%s WHERE id=%s"
@@ -48,9 +48,9 @@ def createStat(userId):
     cursor.execute(query, values) 
     conn.commit()
 
-def createGame(gameid, white_player, black_player, boardstate, completed, time_started):
+def createGame(gameid, white_player, black_player, boardstate, completed, time_started, last_move):
     query = sql['createGame']
-    values = [gameid, white_player, black_player, boardstate, completed, time_started]
+    values = [gameid, white_player, black_player, boardstate, completed, time_started, last_move]
     cursor.execute(query, values)
     conn.commit()
 
