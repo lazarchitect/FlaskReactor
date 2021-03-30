c9ae9ee02722aea4b3b3e36655caa53912715ffc
Eddie Lazar <eddie.lazar@yahoo.com>
2021-03-29 23:09:48 -0400

    fixed up new model classes to mirror db table columns

--- models/User.py
+++ models/User.py
@@ -7,10 +7,9 @@ class User:
     """bare minimum"""
     def __init__(self, username, password_hash):
         self.username = username
-        self.password_hash = password_hash
-        self.userId = utils.generateId()
         self.email = None
-        self.security_q = None
+        self.userId = utils.generateId()
+        self.password_hash = password_hash
 
     """pgdb instantiation"""
     def __init__(self, record):
@@ -19,7 +18,6 @@ class User:
         self.email = record[1]
         self.userId = record[2]
         self.password_hash = record[4]
-        self.security_q = record[5]
 
     def toTuple(self):
-        return (self.username, self.email, self.userId, self.password_hash, self.security_q)
+        return (self.username, self.email, self.userId, self.password_hash)
