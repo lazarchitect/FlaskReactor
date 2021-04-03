741482cdd50cc0b258b42c1c6cc1a59e9a80dda2
Eddie <eddie.lazar@yahoo.com>
2021-04-03 02:17:39 -0400

    split up Board.js into smaller Components

--- static/scripts/Board.js
+++ static/scripts/Board.js
@@ -3,44 +3,55 @@ class Board extends React.Component {
 
 	render() {
 
-		var boardArray = gamestate.tiles // comes right from flask kwargs
+		var boardArray = gamestate.tiles // comes from flask -> jinja -> clientside scope
 		var reactRowArray = []
 		for(var i = 0; i < boardArray.length; i++){
-			reactRowArray.push(this.generateRow(i, boardArray[i]))
+			reactRowArray.push(<Row rowIndex={i} tiles={boardArray[i]} />);
+			console.log(reactRowArray)
 		}
 		return <div className="board">{reactRowArray}</div>
 	}
+    
+}
 
-	generateRow(rowIndex, row){
-
-		var isDarkTile = rowIndex%2==0 ? false : true;
+class Row extends React.Component {
+	
+	render () {
+		
+		var darkTile = this.props.rowIndex % 2 == 0 ? false : true;
+		
+		console.log("rendering row " + this.props.rowIndex)
 
 		var reactTileArray = []
-		for(var tileIndex = 0; tileIndex < row.length; tileIndex++) {
-			reactTileArray.push(this.generateTile(isDarkTile, rowIndex, tileIndex, row[tileIndex]))
-			isDarkTile = !isDarkTile
+		for(var tileIndex = 0; tileIndex < this.props.tiles.length; tileIndex++) {
+			reactTileArray.push(<Tile darkTile={darkTile} rowIndex={this.props.rowIndex} tileIndex={tileIndex} data={this.props.tiles[tileIndex]}/>)
+			darkTile = !darkTile
 		}
 
 		return reactTileArray
 	}
+}
 
-	generateTile(isDarkTile, rowIndex, tileIndex, tile){
+var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-		var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+class Tile extends React.Component {
+	render() {
 
-		var tileId = columns[tileIndex].toString() + (rowIndex+1);
+		console.log("rendering a tile at " + this.props.rowIndex + this.props.tileIndex)
+		
+		var tileId = columns[this.props.tileIndex].toString() + (this.props.rowIndex+1);
 
 		var tileDiv = (
 		<div 
-			key = {tileIndex.toString() + rowIndex.toString()}
-			style = {{left: (tileIndex*60) + "px", top:(rowIndex*60)+"px"}} 
-			className = {isDarkTile ? "tile darkTile" : "tile lightTile"}
+			key = {this.props.tileIndex.toString() + this.props.rowIndex.toString()}
+			style = {{left: (this.props.tileIndex*60) + "px", top:(this.props.rowIndex*60)+"px"}} 
+			className = {this.props.darkTile ? "tile darkTile" : "tile lightTile"}
 			id = {tileId}
 			onClick = {() => document.getElementById(tileId).style.backgroundColor = "blue"}
 		>
 
 			{/* tile contents	 */}
-			<span>{this.pieceInitials(tile)}</span>
+			<span>{this.pieceInitials(this.props.data)}</span>
 		
 		</div>
 		);
@@ -48,10 +59,9 @@ class Board extends React.Component {
 		return tileDiv;
 	}
 
-	pieceInitials(tile){
-		if (tile.piece == null) return;
-		return tile.piece.color[0] + tile.piece.type[0]
+	pieceInitials(data){
+		if (data.piece == null) return;
+		return data.piece.color[0] + data.piece.type[0]
 	}
-
-    
+	
 }
