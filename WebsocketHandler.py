067d57047ea10337aced833491db1edcd974d8ec
Eddie <eddie.lazar@yahoo.com>
2021-05-14 03:29:20 -0400

    added a basic WS handler. more to come!

---
+++ WebsocketHandler.py
@@ -0,0 +1,11 @@
+import tornado.websocket
+
+class ChessWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You just said \"" + message + "\"... lmao wtf?")
+
+    def on_close(self):
+        print("WebSocket closed")
