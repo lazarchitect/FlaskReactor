4a6320b30c6b5fc9514d73a32e52b1f57cb7baa2
Eddie <eddie.lazar@yahoo.com>
2021-08-18 16:40:10 -0400

    ttt html rearranged. onClick now registering

--- static/scripts/tttGame_script.js
+++ static/scripts/tttGame_script.js
@@ -4,13 +4,19 @@ var boardstate = payload.boardstate;
 // TODO DELETE THIS AFTER DEVELOPMENT
 boardstate = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O'];
 
+function logCell(){
+	console.log(row + "/" + index);
+}
+
 function TttBoardRow(props){
 	const row = props.row;
 	return (boardstate.slice(props.start, props.end)).map((cellItem, index) => 
 			<span 
-			key={index} 
-			className="tttCell" 
-			style={{left: ((15*index)+(index+1)*60) + "px", top: (40 + (row*-26) + row*60) + "px"}}>
+				key={index} 
+				className="tttCell" 
+				onClick={() => logCell()}
+				style={{left: 10+((15*index)+(index+1)*60) + "px", top: (-450+row*30) + "px"}}
+			>
 				{cellItem}
 			</span>
 	);
@@ -19,15 +25,18 @@ function TttBoardRow(props){
 function TttBoard(){
 	return (
 		<div id="tttBoard">
-			<TttBoardRow row={0} start={0} end={3}/><br/>
-			<TttBoardRow row={1} start={3} end={6}/><br/>
-			<TttBoardRow row={2} start={6} end={9}/><br/>
-			<svg id="octothorpe" width="500px" height="500px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" z-index={0}>
-  				<rect className="rectLine" x="166" y="50" width="12" height="400" />
-				<rect className="rectLine" x="330" y="50" width="12" height="400" />
-				<rect className="rectLine" x="50" y="166" width="400" height="12" />
-				<rect className="rectLine" x="50" y="330" width="400" height="12" />
+			
+			<svg id="octothorpe" width="500px" height="500px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+  				<rect className="rectLine" x="166" y="50" width="12" height="400" rx="5" />
+				<rect className="rectLine" x="330" y="50" width="12" height="400" rx="5"/>
+				<rect className="rectLine" x="50" y="166" width="400" height="12" rx="5"/>
+				<rect className="rectLine" x="50" y="330" width="400" height="12" rx="5"/>
 			</svg>
+			<div id="tttBoardData">
+				<TttBoardRow z-index={2} row={0} start={0} end={3}/><br/>
+				<TttBoardRow z-index={2} row={1} start={3} end={6}/><br/>
+				<TttBoardRow z-index={2} row={2} start={6} end={9}/><br/>
+			</div>
 		</div>
 	)
 }
