50f53f659330d8812dd5d37c3b7bb19f8c521d0d
Eddie <eddie.lazar@yahoo.com>
2021-08-18 15:56:21 -0400

    TTT board visually working

--- static/scripts/tttGame_script.js
+++ static/scripts/tttGame_script.js
@@ -1,30 +1,39 @@
 
 var boardstate = payload.boardstate;
 
-// sample boardstate
-// ['X', '', 'O', 'O', '', 'X', '', '', '']
-
 // TODO DELETE THIS AFTER DEVELOPMENT
 boardstate = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O'];
 
 function TttBoardRow(props){
-	return (boardstate.slice(props.start, props.end)).map(
-		(cellItem, index) => <span key={index} className="tttCell">{cellItem}</span>
+	const row = props.row;
+	return (boardstate.slice(props.start, props.end)).map((cellItem, index) => 
+			<span 
+			key={index} 
+			className="tttCell" 
+			style={{left: ((15*index)+(index+1)*60) + "px", top: (40 + (row*-26) + row*60) + "px"}}>
+				{cellItem}
+			</span>
 	);
 }
 
 function TttBoard(){
 	return (
 		<div id="tttBoard">
-			<TttBoardRow start={0} end={3}/><br/>
-			<TttBoardRow start={3} end={6}/><br/>
-			<TttBoardRow start={6} end={9}/><br/>
+			<TttBoardRow row={0} start={0} end={3}/><br/>
+			<TttBoardRow row={1} start={3} end={6}/><br/>
+			<TttBoardRow row={2} start={6} end={9}/><br/>
+			<svg id="octothorpe" width="500px" height="500px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" z-index={0}>
+  				<rect className="rectLine" x="166" y="50" width="12" height="400" />
+				<rect className="rectLine" x="330" y="50" width="12" height="400" />
+				<rect className="rectLine" x="50" y="166" width="400" height="12" />
+				<rect className="rectLine" x="50" y="330" width="400" height="12" />
+			</svg>
 		</div>
 	)
 }
 
 var rootElem = (
-	<div id="page">
+	<div id="tttGamePage">
 		<TttBoard/>
 		<form id="logoutButton" action="/logout" method="POST">
 			<input type="submit" value="Log Out"/>
