00481e87b4dd243f3a21cf632d3af5f3c40e3937
Eddie Lazar <eddie.lazar@yahoo.com>
2021-08-21 11:06:10 -0400

    update tttBoard for react state WIP

--- static/scripts/tttGame_script.js
+++ static/scripts/tttGame_script.js
@@ -1,21 +1,17 @@
 
-console.log(payload);
-var boardstate = payload.boardstate;
-
-// DELETE THIS AFTER DEVELOPMENT
-// boardstate = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O'];
-
-function logCell(row, index){
-	console.log(row + "/" + index);
+function tttBoardClick(boardstate) {
+	// websockets!
+	console.log(cellItem);
 }
+
 
 function TttBoardRow(props){
 	const row = props.row;
-	return (boardstate.slice(props.start, props.end)).map((cellItem, index) => 
+	// props values will look like ["X", "X", "O"]. each is a cellItem
+	return (props.values).map((cellItem, index) => 
 			<span 
 				key={index} 
 				className="tttCell" 
-				// onClick={() => }
 				style={{left: 75+(index*150) + "px", top: 70+(row*150) + "px"}}
 			>
 				{cellItem === "" ? " ": cellItem}
@@ -24,8 +20,12 @@ function TttBoardRow(props){
 }
 
 function TttBoard(){
+
+	payload.boardstate = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O'];
+	const [boardstate, setBoardstate] = React.useState(payload.boardstate);
+
 	return (
-		<div id="tttBoard">
+		<div id="tttBoard" onClick={() => tttBoardClick()}>
 			
 			<svg id="octothorpe" width="500px" height="500px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
   				<rect className="rectLine" x="166" y="50" width="12" height="400" rx="5" />
@@ -34,9 +34,9 @@ function TttBoard(){
 				<rect className="rectLine" x="50" y="330" width="400" height="12" rx="5"/>
 			</svg>
 			<div id="tttBoardData">
-				<TttBoardRow z-index={2} row={0} start={0} end={3}/><br/>
-				<TttBoardRow z-index={2} row={1} start={3} end={6}/><br/>
-				<TttBoardRow z-index={2} row={2} start={6} end={9}/><br/>
+				<TttBoardRow row={0} values={boardstate.slice(0,3)} /><br/>
+				<TttBoardRow row={1} values={boardstate.slice(3,6)}/><br/>
+				<TttBoardRow row={2} values={boardstate.slice(6,9)}/><br/>
 			</div>
 		</div>
 	)
