fc0c37f8c7a304c54a028d10dae5bf6eeeba760f
Eddie <eddie.lazar@yahoo.com>
2021-09-11 00:46:26 -0400

    convert Board component to functional

--- static/scripts/chessboard.js
+++ static/scripts/chessboard.js
@@ -1,33 +1,29 @@
-
-class Board extends React.Component {
-
-	render() {
-
-		var boardArray = this.props.boardstate.tiles // comes from flask -> jinja -> clientside scope
-		var reactRowArray = []
-		for(var i = 0; i < boardArray.length; i++){
-			reactRowArray.push(<Row key={i.toString()} rowIndex={i} tiles={boardArray[i]} />);
-		}
-		return <div className="board">{reactRowArray}</div>
+function mountBoard() {
+	// const clientSocket = new WebSocket("ws://100.1.211.86:5000/websocket");
+	const clientSocket = new WebSocket("ws://localhost:5000/websocket");
 		
-	}
+	clientSocket.onmessage = (message) => {
+		console.log("Message from server: ", message.data);
+	};
+	var board = document.getElementsByClassName("board")[0];
+	board.onclick = function(){
+		console.log("click detected: sending message to socketServer.");
+		clientSocket.send("hello, server");
+	};
+}
 
-	
 
-	componentDidMount(){
-		console.log("initializing WS")
-        const clientSocket = new WebSocket("ws://100.1.211.86:5000/websocket")
+function Board(props) {
 
-        clientSocket.onmessage = (message) => {
-			console.log("Message from server: ", message.data);
-        };
+	React.useEffect(() => mountBoard());
 
-        var board = document.getElementsByClassName("board")[0];
-        board.onclick = function(){
-        	console.log("click detected: sending message to socketServer.");
-        	clientSocket.send("hello, server");
-        };
-	}
+	var boardArray = payload.boardstate.tiles;
+
+	return (
+		<div className="board">
+			{boardArray.map((val, i)=><Row key={i.toString()} rowIndex={i} tiles={val}></Row>)}
+		</div>
+	);		
 }
 
 class Row extends React.Component {
