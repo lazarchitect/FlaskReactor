b1a56b82cd4b0d8e8bf94497fb747d0d2c57147f
lazarchitect <eddie.lazar@yahoo.com>
2024-10-07 19:27:38 -0400

    re-add this comment to remind future me about stats updates

--- handlers/tttHandler.py
+++ handlers/tttHandler.py
@@ -180,3 +180,24 @@ class TttHandler(WebSocketHandler):
         utils.updateAll(clientConnections[gameId], message)
 
         self.pgdb.endTttGame(datetime.now(), winner, gameId)
+
+        # userId = fields['userId']
+        # stat = self.pgdb.getStat(userId)
+
+        # ttt_games_played = stat['ttt_games_played'] + 1
+        # ttt_wins = stat['ttt_wins'] + (1 if winner == player else 0)
+        # ttt_win_percent = ttt_wins/ttt_games_played
+        # ttt_played_x = stat['ttt_played_x'] + (1 if player==tttGame.x_player else 0)
+        # ttt_played_o = stat['ttt_played_o'] + (1 if player==tttGame.o_player else 0)
+        # ttt_won_x = stat['ttt_won_x'] + (1 if winner == player and player==tttGame.x_player else 0)
+        # ttt_won_o = stat['ttt_won_o'] + (1 if winner == player and player==tttGame.o_player else 0)
+
+        # self.pgdb.updateTttStat(
+        #     ttt_games_played,
+        #     ttt_wins,
+        #     ttt_win_percent,
+        #     ttt_played_x,
+        #     ttt_played_o,
+        #     ttt_won_x,
+        #     ttt_won_o,
+        #     userId)
