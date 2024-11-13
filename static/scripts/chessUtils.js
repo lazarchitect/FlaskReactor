c1b37e2d18b5a3c5088d17e848775df2ae5d86b6
Eddie Lazar <eddie.lazar@yahoo.com>
2024-11-13 02:04:30 -0500

    adding todos for later

--- static/scripts/chessUtils.js
+++ static/scripts/chessUtils.js
@@ -16,6 +16,7 @@ function isPiece(boardstate, coords, pieceType, pieceColor) {
 // recursive fn to scan along a row/col/diag to see if a given piece is in that direction.
 function pieceTowards(boardstate, coords, offset) {
     let targetCoords = [coords[0] + offset[0], coords[1] + offset[1]]; 
+    // TODO FINISH AS PART OF #78
 }
 
 function inCheck(boardstate, enemyColor, kingCoords) {
@@ -45,10 +46,10 @@ function inCheck(boardstate, enemyColor, kingCoords) {
         return true;
 
     // LOOK FOR ROOKS/QUEENS
-    
     rookOffsets.forEach(offset => {
         let piece = pieceTowards(boardstate, kingCoords, offset);
         if (piece != null) { // is null correct here?
+            // TODO FINISH AS PART OF #78
             // if (pieceMatch(piece, enemyColor, "Rook") || pieceMatch(piece, enemyColor, "Queen")) {
             //     return true;
             // }
@@ -59,12 +60,11 @@ function inCheck(boardstate, enemyColor, kingCoords) {
     bishopOffsets.forEach(offset => {
         let piece = pieceTowards(boardstate, kingCoords, offset)
         if (piece != null){
+            // TODO FINISH AS PART OF #78
             // if (pieceMatch(piece, enemyColor, "Bishop") || pieceMatch(piece, enemyColor, "Queen")){
             //     return true;
             // }
         }
-                
-
     return false;
     })
 }   
