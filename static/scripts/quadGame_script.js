9f66390f7d02b739bc87cc5ff90968718bfb0d6f
lazarchitect <eddie.lazar@yahoo.com>
2025-03-25 19:46:33 -0400

    #55 add basic visuals and file setup, CSS for Quadradius board

--- static/scripts/quadGame_script.js
+++ static/scripts/quadGame_script.js
@@ -1,7 +1,32 @@
 'use strict';
 
+function QuadTile (props) {
+    // console.log(index);
+    // console.log(jk);
+    return <div key={props[0]} className={"quadTile"}></div>
+}
+
+
+function QuadRow() {
+    let tileArray = [];
+    for (let i = 0; i < 10; i++) {
+        tileArray.push(<QuadTile props={[i, 2]}></QuadTile>);
+    }
+    return tileArray;
+}
+
 function QuadBoard(boardstate){
-    return boardstate + "Hello, World!"
+    
+    let rowArray = [];
+    for (let i = 0; i < 8; i++) {
+        rowArray.push(<QuadRow key={i}></QuadRow>);
+    }
+    
+    return (
+        <div id="quadboard" display='inline'>
+            {rowArray}
+        </div>
+    )
 }
 
 var reactRoot = (
