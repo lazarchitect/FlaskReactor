e0b45fcbb360360b15ab5b74685804829e073101
lazarchitect <eddie.lazar@yahoo.com>
2025-07-18 04:14:23 -0400

    adding a ton of stuff for Message Component, logic, appearance

--- src/static/scripts/CommonComponents.js
+++ src/static/scripts/CommonComponents.js
@@ -30,17 +30,36 @@ export function MessageBox (props) {
 	return (
 		<div id="messagebox">
 			
-			<div id="messagebox-main">
-
+			<div id="messagebox-main" style={{visibility: 'hidden'}}>
+				<form type='submit' method='post'>
+					<textarea id="messagebox-textarea" readOnly></textarea>
+					<textarea id="messagebox-input" rows='2' maxLength='68' required 
+					onKeyDown={(e) => {
+						console.log(e);
+						if (e.key == 'Enter') {
+							form.submit();
+							// TODO:
+							// send off message to Message websocket
+							// add user message to message crawl
+							// await receival of any messages coming in from websocket, to add to crawl as well.
+						}
+					}}>
+							
+					</textarea>
+				</form>
 			</div>
-			<div id="messagebox-base" 
+
+			<div id="messagebox-base">
+				<span id='messagebox-text'>Chatbox</span>
+				<span id='messagebox-indicator'
 					onClick={() => {
 						let currentVis = document.getElementById('messagebox-main').style.visibility;
 						let newVis = currentVis == 'hidden' ? 'visible' : 'hidden';
+						let newText = currentVis == 'hidden' ? 'Hide' : 'Expand';
 						document.getElementById('messagebox-main').style.visibility = newVis;
-						
-						}}>
-				<p><span id='chatbox-indicator'>Expand</span> Chatbox</p>
+						document.getElementById('chatbox-indicator').innerText = newText;
+					}}>
+					Expand</span>
 			</div>
 
 		</div>
