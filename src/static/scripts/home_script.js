8d9736fa2a8f4d76c7faf52ebd61786ab4f1d06c
lazarchitect <eddie.lazar@yahoo.com>
2025-07-23 04:23:30 -0400

    # Completed Ttt games are now hidden by default and appear or disappear on checkbox click

--- src/static/scripts/home_script.js
+++ src/static/scripts/home_script.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-import React from 'react'; // do I need this?
+import React, { StrictMode } from 'react'; // do I need this?
 import { createRoot } from 'react-dom/client';
 import { SiteHeader } from './CommonComponents';
-import styled  from 'styled-components';
+import styled from 'styled-components';
 
 var chessGames = payload.chessGames;
 var username = payload.username;
@@ -26,42 +26,61 @@ function openGame(gameId, gameType){
 	window.location.href = "/games/" + gameType + "/" + gameId;
 }
 
-// chessGames comes from Flask -> html script
-var activeChessGames = chessGames.map((game) => 
-
-	game[4] == false ?
-
+function ChessGameList (props) {
+	return chessGames
+	.filter(game => game[4] == props.completed)
+	.map(game => 
 		<GameDiv className="chessGame" tabIndex={"0"} key={game[0]} onClick={() => openGame(game[0], "chess")}>
 			{"Vs. " + (game[1] === username ? game[2] : game[1])}
 		</GameDiv>
-	: ""
-);
+	);
+}
 
-var activeTTTGames = tttGames.map((game) => {
+function ChessGames (props) {
 
-	let completed = game[3];
-	
-	if (!completed) {
-		return (<GameDiv className="tttGame" tabIndex={"0"} key={game[0]} onClick={() => openGame(game[0], "ttt")}>
-				{"Vs. " + (game[1] === username ? game[2] : game[1])}
-			</GameDiv>);
-	}
-	return "";
-	}
-);
+	return (
+		<div>
+			<h4>Chess Games:</h4>			
+			<ChessGameList completed={false}/>
+		</div>
+	);
+}
 
-var completedTTTGames = tttGames.map((game) => {
+function TttGameList (props) {
+	return tttGames
+	.filter(game => game[3] == props.completed)
+	.map(game => 
 
-	let completed = game[3];
-	
-	if (completed) {
-		return (<GameDiv className="tttGame" tabIndex={"0"} key={game[0]} onClick={() => openGame(game[0], "ttt")}>
-				{"Vs. " + (game[1] === username ? game[2] : game[1])}
-			</GameDiv>);
-	}
-	return "";
-	}
-);
+		<GameDiv tabIndex={"0"} key={game[0]} onClick={() => openGame(game[0], "ttt")}>
+			{"Vs. " + (game[1] === username ? game[2] : game[1])}
+		</GameDiv>
+	);
+}
+
+function enableOnClick() {
+	document.getElementById('viewPastTttGames').onclick = (() => {
+		let currentVisbility = document.getElementById("pastTttGames").style.visibility;
+		document.getElementById("pastTttGames").style.visibility = (currentVisbility == 'visible' ? 'hidden' : 'visible');
+	});
+}
+
+function TttGames (props) {
+
+	React.useEffect(() => enableOnClick(), []);
+
+	return (
+		<div>
+			<h4>TicTacToe Games:</h4>
+			<TttGameList completed={false}/>
+			Show Past Games? <input type="checkbox" id="viewPastTttGames"/>
+			<div id="pastTttGames">
+				<TttGameList completed={true}/>
+			</div>
+		</div>
+	);
+}
+
+
 
 const gameTypes = ["Chess", "Tic-Tac-Toe"]; 
 
@@ -75,27 +94,19 @@ var page = (
 		<form action="/creategame" method="POST" id="createGameDiv">
 			<h4>Create Game</h4>
 			
-			
 			<select name="gameType"> {/*The name attribute is used to reference the form data*/}
   				{jsxGT}
 			</select>
 			<br/>
 			
-			Opponent Username: <input type="text" name="opponent" required /> 
-			<input type="submit" value="Create" />
+			Opponent Username: <input type="text" name="opponent"/> 
+			<input type="submit" value="Create"/>
 		</form>
 
-		<h4>Chess Games:</h4> {activeChessGames}
+		<ChessGames/>
+		<TttGames/>
 
-		View Past Games? <input type="checkbox" id="viewPastChessGames"/>
-
-		<h4>TicTacToe Games:</h4> {activeTTTGames}
-
-		View Past Games? <input type="checkbox" id="viewPastTttGames"/>
-
-		{document.getElementById('viewPastTttGames').checked ? completedTTTGames : ""}
-
-		// TODO react should manage the state of the games divs and we call setState when the checkboxes are clicked 
+		{/* TODO react should manage the state of the games divs and we call setState when the checkboxes are clicked  */}
 
 	</div>
 );
