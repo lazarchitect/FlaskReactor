867a2ec6f95020217d1bc2fcfcae716d05a1de29
lazarchitect <eddie.lazar@yahoo.com>
2025-11-07 00:44:40 -0500

    expand MockPgdb capabilities a little bit to support an actual chessboard

--- src/MockPgdb.py
+++ src/MockPgdb.py
@@ -3,6 +3,8 @@ from src.models.User import User
 from src.models.Stats import Stats
 from src.models.Message import Message
 
+chessJson = [[{"piece":{"row":0,"col":0,"type":"Rook","color":"Black","id":"br1"}},{"piece":{"row":0,"col":1,"type":"Knight","color":"Black","id":"bn1"}},{"piece":{"row":0,"col":2,"type":"Bishop","color":"Black","id":"bb1"}},{"piece":{"row":0,"col":3,"type":"Queen","color":"Black","id":"bq"}},{"piece":{"row":0,"col":4,"type":"King","color":"Black","id":"bk"}},{"piece":{"row":0,"col":5,"type":"Bishop","color":"Black","id":"bb2"}},{"piece":{"row":0,"col":6,"type":"Knight","color":"Black","id":"bn2"}},{"piece":{"row":0,"col":7,"type":"Rook","color":"Black","id":"br2"}}],[{"piece":{"row":1,"col":0,"type":"Pawn","color":"Black","id":"bp1"}},{"piece":{"row":1,"col":1,"type":"Pawn","color":"Black","id":"bp2"}},{"piece":{"row":1,"col":2,"type":"Pawn","color":"Black","id":"bp3"}},{"piece":{"row":1,"col":3,"type":"Pawn","color":"Black","id":"bp4"}},{"piece":{"row":1,"col":4,"type":"Pawn","color":"Black","id":"bp5"}},{"piece":{"row":1,"col":5,"type":"Pawn","color":"Black","id":"bp6"}},{"piece":{"row":1,"col":6,"type":"Pawn","color":"Black","id":"bp7"}},{"piece":{"row":1,"col":7,"type":"Pawn","color":"Black","id":"bp8"}}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{"piece":{"row":6,"col":0,"type":"Pawn","color":"White","id":"wp1"}},{"piece":{"row":6,"col":1,"type":"Pawn","color":"White","id":"wp2"}},{"piece":{"row":6,"col":2,"type":"Pawn","color":"White","id":"wp3"}},{"piece":{"row":6,"col":3,"type":"Pawn","color":"White","id":"wp4"}},{"piece":{"row":6,"col":4,"type":"Pawn","color":"White","id":"wp5"}},{"piece":{"row":6,"col":5,"type":"Pawn","color":"White","id":"wp6"}},{"piece":{"row":6,"col":6,"type":"Pawn","color":"White","id":"wp7"}},{"piece":{"row":6,"col":7,"type":"Pawn","color":"White","id":"wp8"}}],[{"piece":{"row":7,"col":0,"type":"Rook","color":"White","id":"wr1"}},{"piece":{"row":7,"col":1,"type":"Knight","color":"White","id":"wn1"}},{"piece":{"row":7,"col":2,"type":"Bishop","color":"White","id":"wb1"}},{"piece":{"row":7,"col":3,"type":"Queen","color":"White","id":"wq"}},{"piece":{"row":7,"col":4,"type":"King","color":"White","id":"wk"}},{"piece":{"row":7,"col":5,"type":"Bishop","color":"White","id":"wb2"}},{"piece":{"row":7,"col":6,"type":"Knight","color":"White","id":"wn2"}},{"piece":{"row":7,"col":7,"type":"Rook","color":"White","id":"wr2"}}]]
+
 class MockPgdb:
 
 	def getUser(self, username):
@@ -24,13 +26,10 @@ class MockPgdb:
 		pass
 
 	def getChessGame(self, gameId):
-		return ChessGame.dbLoad(['FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', [], False, None, None, None, 'MockUser1', None,None,None,None,None,None,None,None,None, -1])
-
-	def getActiveChessGames(self, username):
-		return [('FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', {}, False, None, None, None, 'MockUser1')]
-
+		return ChessGame.dbLoad(['FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', chessJson, False, None, None, None, 'MockUser1', None,None,None,None,None,None,None,None,None, -1])
+		
 	def getChessGames(self, username):
-		return [('FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', {}, False, None, None, None, 'MockUser1')]
+		return [('FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', chessJson, False, None, None, None, 'MockUser1')]
 
 	def getTttGames(self, username):
 		return [('FAKE8dd7-e30d-42d7-8c3a-94483dfc4af3', 'MockUser1', 'MockUser2', {}, False, None, None, None, 'MockUser1')]
