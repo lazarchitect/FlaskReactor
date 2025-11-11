6b42e1e37041a194182d6326bc6c4ad2f4a2b8df
lazarchitect <eddie.lazar@yahoo.com>
2025-11-11 03:01:51 -0500

    #92 build out changes for drag and drop (long dev time) using react-dnd and react boardstate state updating

--- src/static/scripts/Torus.js
+++ src/static/scripts/Torus.js
@@ -1,48 +1,23 @@
 
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
 
-export function Torus (props) {
+export function Torus ({ torus, row, col }) {
     
-    const [position, setPosition] = React.useState({x:0, y:0});
-    const [dragStart, setDragStart] = React.useState({x:0, y:0});
-    const [isDragging, setIsDragging] = React.useState(false);
+    const [ { opacity }, dragRef, dragPreviewRef ] = useDrag(
+        () => ({
+            type: 'Torus',
+            item: { torus: torus, dragRow: row, dragCol: col }, // gets passed to drop function
+            collect: (monitor) => ({
+                opacity: monitor.isDragging() ? 0.6 : 1
+            })
+        }),
+        []
+    );
+
+    // TODO edit torus image based on team color (and eventually, powerups).
+    // can use SVG for this? or <canvas> pixel manipulation for the colors / adding elements, layers, etc
     
-    return <div
-            id = {{}}
-            className="torus"
-            style={{
-                cursor: "move",
-                left: position.x,
-                top: position.y
-            }}
-
-            onMouseDown={(e) => {
-                setIsDragging(true);
-                setDragStart({x: e.clientX, y: e.clientY});
-            }}
-            onMouseUp={(e) => setIsDragging(false)}
-            /* ALERT! MOUSE UP WILL ONLY TRIGGER IF YOU MOUSEUP _WHILE_ OVER THE DIV! 
-            SO YOU NEED TO TRACK MOUSE EVENTS OF THE WHOLE PAGE..? */
-
-            onMouseMove={(e) => {
-
-                if (!isDragging) {
-                    return;
-                }
-
-                console.log("dragStart:" + dragStart.x + " "+  dragStart.y );
-                console.log("position:" + position.x + " " + position.y);
-
-                setPosition({
-                    x: e.clientX - dragStart.x, // always bounces back to origin since mouseDown sets dragstart to client, so this evals to 0,0
-                    y: e.clientY - dragStart.y 
-                });
-                // console.log(position);
-            }}
-            
-        >
-        <img className="torusImg" src="/static/images/quadradius/torus_default.png"></img>
-    </div>
-    
+    return <div className="torus" style={{cursor: "move", opacity: opacity}} ref={dragRef}></div>;
 }
--- src/static/scripts/quadGame_script.js
+++ src/static/scripts/quadGame_script.js
@@ -1,53 +1,110 @@
 'use strict';
 
-import React from 'react'; // do I need this?    yes
+import React from 'react'; // required by React
 import { createRoot } from 'react-dom/client';
+
+import { DndProvider, useDrop } from "react-dnd";
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import { SiteHeader } from './CommonComponents';
 import { Torus } from './Torus';
 
-<div />
+// TODO definitely extract these functional components into their own file(s) and then have game logic live in other files
+// this file is just for grabbing the QuadBoard and other elements on the screen (chat, menu, etc)
 
-function QuadTile (props) {
+function QuadTile ({ rowIndex, columnIndex, tileData }) {
 
-    if (props.tileData.piece != undefined) {
-        return <div className="quadTile"><Torus piece={props.tileData.piece}></Torus></div> 
+
+    let tileId = "tile_"+rowIndex+"_"+columnIndex;
+    
+    if ("torus" in tileData) {
+        return (
+            <div id={tileId} className="quadTile">
+                <Torus torus={tileData.torus} row={rowIndex} col={columnIndex}></Torus>
+            </div>
+        );
     }
 
-    return <div className="quadTile"></div>
+    return (<div id={tileId} className="quadTile"></div>);
 }
 
 
-function QuadRow(props) {
+function QuadRow({ rowIndex, rowData }) {
     let tileArray = [];
-    for (let tileIndex = 0; tileIndex < 10; tileIndex++) {
-        tileArray.push(<QuadTile key={tileIndex} tileData={props.rowData[tileIndex]}></QuadTile>);
+    for (let columnIndex = 0; columnIndex < 10; columnIndex++) {
+        tileArray.push(<QuadTile key={columnIndex} columnIndex={columnIndex} rowIndex={rowIndex} tileData={rowData[columnIndex]}></QuadTile>);
     }
     return tileArray;
 }
 
-function QuadBoard(props){
+function QuadBoard(){
+
+    const [boardstate, setBoardstate] = React.useState(payload.boardstate);
+
+    const [{isOver, _}, dropRef] = useDrop(() => ({
+        accept: 'Torus',
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop()
+        }),
+        drop: ((item, monitor) => {
+            
+            // TODO extract to helper method?
+            let rec = document.getElementById("quadboard").getBoundingClientRect();
+            let mousePos = monitor.getClientOffset();
+            let boardStartX = rec.left + window.scrollX;
+            let boardStartY = rec.top + window.scrollY;
+            
+            let [boardClickX, boardClickY] = [mousePos.x - boardStartX, mousePos.y - boardStartY];
+
+            let cellWidth = rec.width / 10;
+            let dropCol = Math.floor(boardClickX / cellWidth);
+
+            let cellHeight = rec.height / 8;
+            let dropRow = Math.floor(boardClickY / cellHeight);
+
+            let targetTile = boardstate[dropRow][dropCol];
+            let sourceTile = boardstate[item.dragRow][item.dragCol];
+
+            if (
+                ("torus" in targetTile && targetTile.torus.color == item.torus.color) // target is occupied with ally 
+                || (targetTile === sourceTile) // cant drop to same location.
+            ) {
+                return;
+            }
+
+            targetTile.torus = item.torus;
+            delete boardstate[item.dragRow][item.dragCol].torus;
+
+            setBoardstate(boardstate);
+
+        })
+    }));
+
     
     let rowArray = [];
     for (let rowIndex = 0; rowIndex < 8; rowIndex++) {
-        rowArray.push(<QuadRow key={rowIndex} rowData={props.boardstate[rowIndex]}></QuadRow>);
+        rowArray.push(<QuadRow key={rowIndex} rowIndex={rowIndex} rowData={boardstate[rowIndex]}></QuadRow>);
     }
     
     return (
-        <div id="quadboard" display='inline'>
+        <div id="quadboard" display='inline' ref={dropRef}>
             {rowArray}
         </div>
     )
 }
 
 var page = (
-	<div id="reactRoot" onMouseMove={(e) => console.log(e.clientX, e.clientY)}>
+	<div id="reactRoot">
 
         {/* top of the page */}
         <SiteHeader version={payload.deployVersion} username={payload.username}/>
         
         {/* rest of the page */}
-        <div id="quadPlayArea">
-            <QuadBoard boardstate={payload.boardstate}/>
+        <div id="quadPlayArea"> 
+            <DndProvider backend={HTML5Backend}>
+                <QuadBoard boardstate={payload.boardstate}/>
+            </DndProvider>
             <p>Status: <span id="status"></span></p>
         </div>
         
