c7c4c9b2192674e817d86b55a6f0eed7dfdeb4c9
lazarchitect <eddie.lazar@yahoo.com>
2025-11-23 04:11:33 -0500

    added example audio for torus drag start

--- src/static/scripts/Torus.js
+++ src/static/scripts/Torus.js
@@ -1,26 +1,33 @@
 
 import React, { useEffect } from 'react';
 import { useDrag } from 'react-dnd';
-import { TorusSVG } from './TorusSVG';
 import { getEmptyImage } from 'react-dnd-html5-backend';
+import useSound from 'use-sound';
+
+import { TorusSVG } from './TorusSVG';
 
 export function Torus ({ torus, row, col }) {
+
+    let [playPickupSound] = useSound('/static/sounds/pickup.wav');
+
+    // useState will be used here for color, powers, buffs, and debuffs later
     
-    const [ { opacity }, dragRef, dragPreviewRef ] = useDrag(
+    const [{ isDragging, opacity }, dragRef, dragPreview] = useDrag(
         () => ({
             type: 'Torus',
             item: { torus: torus, dragRow: row, dragCol: col }, // gets passed to drop function
             collect: (monitor) => ({
+                isDragging: monitor.isDragging(),
                 opacity: monitor.isDragging() ? 0.6 : 1
             })
         }),
         [torus] // react drag systems update their own records when this changes
     );
-    
-    return <div className='torus' style={{cursor: "grab", opacity: opacity}} ref={dragRef}>
-        <TorusSVG color={torus.color}/>
-    </div>;
-}
+
+
+    if (isDragging) {
+        playPickupSound();
+    }
 
 
     // following code removes default browser Torus image during drag. 
