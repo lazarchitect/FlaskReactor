07c7aa8f2af1e46cd076da81dfb3a998c63b331b
lazarchitect <eddie.lazar@yahoo.com>
2026-01-02 00:03:21 -0500

    add Vs. {opponentName} to Chess page

--- src/static/scripts/roots/chessGameRoot.js
+++ src/static/scripts/roots/chessGameRoot.js
@@ -6,13 +6,17 @@ import { SiteHeader } from '../components/common/SiteHeader';
 import { Chatbox } from '../components/common/Chatbox';
 import { Chessboard } from '../components/chess/chessboard';
 
-let isPlayer = payload.players.includes(payload.username);
+let {players} = payload;
+
+let isPlayer = players.includes(payload.username);
+let opponentName = isPlayer ? players.filter(player => player.username !== payload.username).pop() : null;
 
 let page = (
 	<>
         <SiteHeader />
         <main>
             <div className="playArea">
+                {isPlayer && <h2>Vs. {opponentName}</h2>}
                 <Chessboard />
                 <p>Status: <span id="status"></span></p>
             </div>
