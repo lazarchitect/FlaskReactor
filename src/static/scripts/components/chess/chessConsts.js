5803cbd5c01f1226dc82f09127e2fbb4621df2d0
lazarchitect <eddie.lazar@yahoo.com>
2026-01-02 00:02:43 -0500

    make OFFSETS consts more readable with named object fields

--- src/static/scripts/components/chess/chessConsts.js
+++ src/static/scripts/components/chess/chessConsts.js
@@ -1,9 +1,13 @@
+const KNIGHT_OFFSET_DIFFS = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]];
+const BISHOP_OFFSET_DIFFS = [[1,1],[1,-1],[-1,1],[-1,-1]];
+const ROOK_OFFSET_DIFFS = [[0,1],[0,-1],[1,0],[-1,0]];
 
+const offsetMap = (diff) => ({row: diff[0], col: diff[1]});
 
-export const KNIGHT_OFFSETS = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]];
-export const BISHOP_OFFSETS = [[1,1],[1,-1],[-1,1],[-1,-1]];
-export const ROOK_OFFSETS = [[0,1],[0,-1],[1,0],[-1,0]];
-export const ROYAL_OFFSETS = [[1,1],[1,-1],[-1,1],[-1,-1],[0,1],[0,-1],[1,0],[-1,0]];
+export const KNIGHT_OFFSETS = KNIGHT_OFFSET_DIFFS.map(offsetMap);
+export const BISHOP_OFFSETS = BISHOP_OFFSET_DIFFS.map(offsetMap);
+export const ROOK_OFFSETS = ROOK_OFFSET_DIFFS.map(offsetMap);
+export const ROYAL_OFFSETS = BISHOP_OFFSETS.concat(ROOK_OFFSETS);
 
 export const Colors = Object.freeze({
     WHITE: 'White',
