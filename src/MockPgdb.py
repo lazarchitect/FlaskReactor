dae4fc5792936ca2dec80b99b854044b06672102
lazarchitect <eddie.lazar@yahoo.com>
2026-06-02 23:48:28 -0400

    add some improvements to MockPgdb (but it will never be enough)

--- src/MockPgdb.py
+++ src/MockPgdb.py
@@ -1,12 +1,17 @@
-from src.models.ChessGame import ChessGame
+import json
 
-chessJson = [[{"piece":{"row":0,"col":0,"type":"Rook","color":"Black","id":"br1"}},{"piece":{"row":0,"col":1,"type":"Knight","color":"Black","id":"bn1"}},{"piece":{"row":0,"col":2,"type":"Bishop","color":"Black","id":"bb1"}},{"piece":{"row":0,"col":3,"type":"Queen","color":"Black","id":"bq"}},{"piece":{"row":0,"col":4,"type":"King","color":"Black","id":"bk"}},{"piece":{"row":0,"col":5,"type":"Bishop","color":"Black","id":"bb2"}},{"piece":{"row":0,"col":6,"type":"Knight","color":"Black","id":"bn2"}},{"piece":{"row":0,"col":7,"type":"Rook","color":"Black","id":"br2"}}],[{"piece":{"row":1,"col":0,"type":"Pawn","color":"Black","id":"bp1"}},{"piece":{"row":1,"col":1,"type":"Pawn","color":"Black","id":"bp2"}},{"piece":{"row":1,"col":2,"type":"Pawn","color":"Black","id":"bp3"}},{"piece":{"row":1,"col":3,"type":"Pawn","color":"Black","id":"bp4"}},{"piece":{"row":1,"col":4,"type":"Pawn","color":"Black","id":"bp5"}},{"piece":{"row":1,"col":5,"type":"Pawn","color":"Black","id":"bp6"}},{"piece":{"row":1,"col":6,"type":"Pawn","color":"Black","id":"bp7"}},{"piece":{"row":1,"col":7,"type":"Pawn","color":"Black","id":"bp8"}}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{},{},{},{},{},{},{},{}],[{"piece":{"row":6,"col":0,"type":"Pawn","color":"White","id":"wp1"}},{"piece":{"row":6,"col":1,"type":"Pawn","color":"White","id":"wp2"}},{"piece":{"row":6,"col":2,"type":"Pawn","color":"White","id":"wp3"}},{"piece":{"row":6,"col":3,"type":"Pawn","color":"White","id":"wp4"}},{"piece":{"row":6,"col":4,"type":"Pawn","color":"White","id":"wp5"}},{"piece":{"row":6,"col":5,"type":"Pawn","color":"White","id":"wp6"}},{"piece":{"row":6,"col":6,"type":"Pawn","color":"White","id":"wp7"}},{"piece":{"row":6,"col":7,"type":"Pawn","color":"White","id":"wp8"}}],[{"piece":{"row":7,"col":0,"type":"Rook","color":"White","id":"wr1"}},{"piece":{"row":7,"col":1,"type":"Knight","color":"White","id":"wn1"}},{"piece":{"row":7,"col":2,"type":"Bishop","color":"White","id":"wb1"}},{"piece":{"row":7,"col":3,"type":"Queen","color":"White","id":"wq"}},{"piece":{"row":7,"col":4,"type":"King","color":"White","id":"wk"}},{"piece":{"row":7,"col":5,"type":"Bishop","color":"White","id":"wb2"}},{"piece":{"row":7,"col":6,"type":"Knight","color":"White","id":"wn2"}},{"piece":{"row":7,"col":7,"type":"Rook","color":"White","id":"wr2"}}]]
+from src.models.QuadradiusGame import QuadradiusGame
+from src.models.ChessGame import ChessGame
+from src.models.User import User
+
+chessJson = json.loads(open('resources/initialChessLayout.json', 'r').read())
+quadJson =json.loads(open('resources/initialQuadLayout.json', 'r').read())
 
 # ALERT this class is just constantly out of date, avoid using at all if possible
 class MockPgdb:
 
 	def getUser(self, username):
-		return ('MockSmith11', 'mock_email@aol.com', 'mock_38298dd7-e30d-42d7-8c3a-94483dfc4af3', 'mock_pw_shtjteykyjthehu6w57', 'mock_wstoken')
+		return User('MockSmith11', 'mock_email@aol.com', 'mock_38298dd7-e30d-42d7-8c3a-94483dfc4af3', 'mock_pw_shtjteykyjthehu6w57', 'mock_wstoken')
 
 	def createUser(self, username, password_hash, email, userid):
 		pass
@@ -43,3 +48,6 @@ class MockPgdb:
 
 	def getMessages(self, gameId):
 		return [(1, "MockUser1", "hello")]
+
+	def getQuadradiusGame(self, gameId):
+		return QuadradiusGame("jim", "jon", "red", "blue", "jim", False)
