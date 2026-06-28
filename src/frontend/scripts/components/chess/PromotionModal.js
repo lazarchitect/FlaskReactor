e5013e19a682ffa9a5cee3f124871e7e3648ab02
Eddie Lazar <eddie.lazar@yahoo.com>
2026-06-28 04:43:49 -0400

    #144 PromotionModal component rendering logic, visual adjustments, and onClick finalized

--- src/frontend/scripts/components/chess/PromotionModal.js
+++ src/frontend/scripts/components/chess/PromotionModal.js
@@ -1,44 +1,50 @@
-import React from "react";
+import React, {useContext} from "react";
 import {BISHOP, BLACK, KNIGHT, QUEEN, ROOK, WHITE} from "./chessConsts";
+import {generatePieceMetadata} from "./chessUtils";
+import {sendPromotionMoveUpdate} from "./chessSocket";
+import {ActivePieceContext} from "./Chessboard";
 
 export default function PromotionModal({tileId}) {
 
-    let row = parseInt(tileId['0']);
+    let [row, col] = [parseInt(tileId['0']), parseInt(tileId['1'])];
 
     const pieces = [BISHOP, KNIGHT, QUEEN, ROOK];
     const pieceColor = row === 7 ? BLACK : WHITE;
 
-    // following code slides element over if it went off left side of screen
+    let initialLeft = -93;
+    let left = initialLeft + (60*col);
+
+    let top = pieceColor === WHITE ? -40 : 450;
+
+    // following code "retrieves" the modal if it initially rendered off left side of screen
     React.useEffect(() => {
 
-        let promotionModal = document.querySelector(".promotionModal");
+        let promotionModal = document.getElementById("promotionModal");
         let distanceToViewportLeft = promotionModal.getBoundingClientRect().left;
         let leftSpillover = Math.min(distanceToViewportLeft, 0);
+        if (leftSpillover < 0) left -= leftSpillover;
 
-        let desiredLeft = -95;
-
-        if (leftSpillover < 0) {
-            desiredLeft -= leftSpillover;
-            promotionModal.style.left = `${desiredLeft}px`;
-        }
+        promotionModal.style.left = `${left}px`;
     }, []);
 
     return (
-        <div className={"promotionModal" + " promotionModal" + pieceColor} >
-            { pieces.map(pieceType => <PromotionPiece key={pieceType} piece={{color: pieceColor, type: pieceType}} /> )}
+        <div id={"promotionModal"} className={"promotionModal" + pieceColor} style={{left: `${left}px`, top: `${top}px`}} >
+            { pieces.map(pieceType => <PromotionChoice key={pieceType} tileId={tileId} piece={{color: pieceColor, type: pieceType}} /> )}
         </div>
     );
 }
 
-function PromotionPiece ({piece}) {
+function PromotionChoice ({piece, tileId}) {
 
-    let imagePath = "/frontend/images/" + piece.color + piece.type + ".png";
-    let altText = "A " + piece.color + " " + piece.type + ".";
+    const activePieceInfo = useContext(ActivePieceContext);
 
-    if (piece.type === "Bishop" && piece.color === "Black") {
-        imagePath = "/frontend/svg/" + piece.color + piece.type + ".svg";
-    }
-    return <div className={"promotionPieceDiv" + " promotionPieceDiv" + piece.color}>
+    let onClick = () => {
+        sendPromotionMoveUpdate(tileId, activePieceInfo.tileId, piece.type);
+    };
+
+    let {imagePath, altText} = generatePieceMetadata(piece);
+
+    return <div onClick={onClick} id={"promotionPieceDiv"} className={"promotionPieceDiv" + piece.color}>
         <img src={imagePath} className="promotionPiece" alt={altText} />
     </div>;
 }
