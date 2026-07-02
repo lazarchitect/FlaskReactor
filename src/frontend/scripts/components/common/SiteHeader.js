09248b0cbd60a3b42d34618df8b40b77d288dc98
Eddie Lazar <eddie.lazar@yahoo.com>
2026-07-02 14:55:48 -0400

    #151 refactor settingsPane code into its own file for organization purposes

--- src/frontend/scripts/components/common/SiteHeader.js
+++ src/frontend/scripts/components/common/SiteHeader.js
@@ -1,7 +1,8 @@
 'use strict';
 
-import React, {useState} from 'react';
+import React from 'react';
 import {SettingsGearSVG} from "./SettingsGearSVG";
+import {SettingsPane} from "./SettingsPane";
 
 export function SiteHeader () {
 
@@ -44,64 +45,8 @@ export function SiteHeader () {
 	)
 }
 
-function SettingsPane({isLoggedIn}) {
-
-	let {preferences} = payload;
-
-	const [quadColorPref, setQuadColorPref] = useState(preferences?.quadColorPref);
-	const [quadColorBackup, setQuadColorBackup] = useState(preferences?.quadColorBackup);
-
-	return <div id="settingsPane">
-		Settings
-		<br/>
-		{isLoggedIn?
-			<>
-				<span>Quadradius Color Preference: </span>
-				<QuadColorSelector command="quadColorPref" setter={setQuadColorPref} value={quadColorPref} />
-				<br/>
-				<span>Quadradius Color Backup: </span>
-				<QuadColorSelector command="quadColorBackup" setter={setQuadColorBackup} value={quadColorBackup} />
-				<br/>
-				<span>Opt out of chat? -- yes/no slider</span>
-			</>
-		: //  else - settings visible while logged out? uses cookies?
-			<></>
-		}
-	</div>
-}
-
-function QuadColorSelector({value, setter, command}) {
-	const quadColors = ["red", "blue", "green", "teal", "orange", "purple"];
-	return <select value={value}
-			onChange={(e) => {
-				setter(e.target.value);
-				updateSettings(command, {"color": e.target.value})
-			}} >
-		{quadColors.map((item) =>
-			<option key={item} value={item}>
-				{item}
-			</option>)
-		}
-	</select>
-}
-
 function formatMax20(username) {
 	if (username.length > 20) return username.slice(0, 17) + "...";
 	return username;
 }
 
-function updateSettings(command, settingsData) {
-	fetch("/receive_settings", {
-		method: "PATCH",
-		headers: {"Content-Type": "application/json"},
-		body: JSON.stringify({
-			"command": command,
-			"username": payload.username,
-			// TODO need to authenticate this user somehow
-			"data": settingsData})
-	})
-	.then(response => {
-		if (response.statusText !== "ACCEPTED") alert("Settings update failed.");
-	});
-
-}
