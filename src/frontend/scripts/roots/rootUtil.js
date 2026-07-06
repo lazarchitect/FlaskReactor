212c4513c09a77779fe4a2477ebee3453493222d
Eddie Lazar <eddie.lazar@yahoo.com>
2026-07-06 04:49:25 -0400

    fix lil bug with player name displaying

--- src/frontend/scripts/roots/rootUtil.js
+++ src/frontend/scripts/roots/rootUtil.js
@@ -2,11 +2,11 @@
  * Based on a game's players, displays the player vs. player text in the page title (tab text)
  * @returns {string} the same text for use later in the page
  */
-export function configureTitleAddition(players) {
-    const isPlayer = players.includes(payload.username);
-    const opponentName = isPlayer ? players.filter(player => player.username !== payload.username).pop() : null;
+export function configureTitleAddition(playerNames) {
+    const isPlayer = playerNames.includes(payload.username);
+    const opponentName = isPlayer ? playerNames.filter(playerName => playerName !== payload.username).pop() : null;
 
-    const titleAddition = " " + (isPlayer ? "Vs. " + opponentName : players[0] + " Vs. " + players[1]);
+    const titleAddition = " " + (isPlayer ? "Vs. " + opponentName : playerNames[0] + " Vs. " + playerNames[1]);
     document.querySelector("title").innerText += titleAddition;
     return titleAddition;
 }
