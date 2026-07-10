0bac9b8137fbee1101b392dd7fee6de587e5fb57
Eddie Lazar <eddie.lazar@yahoo.com>
2026-07-10 13:30:51 -0400

    add abstract WebSocketHandler class for generic connection handling logic

---
+++ src/backend/handlers/AbstractWebSocketHandler.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+from tornado.websocket import WebSocketHandler, WebSocketClosedError
+
+from src.backend.pgdb import getPgdb
+from src.backend.utils import generateId
+
+
+class AbstractWebSocketHandler(WebSocketHandler):
+	clientConnections = None  # each subclass initializes this
+
+	def initialize(self):
+		self.pgdb = getPgdb()
+		self.gameId = None # created later, during subscribe
+		self.handlerType = None # initialized by subclass
+
+	# this fn is required due to Flask/Tornado rejecting unspecified origins as Forbidden
+	# unless we allow it explicitly
+	def check_origin(self, origin):
+		return True
+
+	def open(self):
+		self.socketId = "socket"+ str(generateId())[:8]
+		logging.info(f"{self.handlerType}Socket opened: " + self.socketId)
+
+	def on_close(self):
+		if not hasattr(self, "gameId"):
+			print(f"{self.handlerType}Socket was not subscribed? not sure why this would happen")
+			return
+		self.deleteConnection(self.gameId, self.socketId)
+		logging.info(f"{self.handlerType}Socket closed: " + self.socketId)
+
+	# TODO wipe gameID from the dict entirely if it is empty after removal of connection
+	def deleteConnection(self, gameId, socketId):
+		gameConnectionList = self.clientConnections[gameId]
+		for x in gameConnectionList:
+			if x['id'] == socketId:
+				gameConnectionList.remove(x)
+				return
+
+	def getClientConnections(self): # DEBUG
+		return self.clientConnections
+
+	def updateAll(self, gameId, message):
+		gameConnections = self.clientConnections[gameId]
+		for connectionDetails in gameConnections:
+			try:
+				connectionDetails['conn'].write_message(json.dumps(message))
+			except WebSocketClosedError:
+				pass
