b5c289a5982b42cb1cb70ea58dc79f131d2679c3
Eddie Lazar <eddie.lazar@yahoo.com>
2026-07-11 17:44:40 -0400

    #163 build ResignButton with proper React state and handling of text, color, and behavior

--- src/frontend/scripts/roots/chessGameRoot.js
+++ src/frontend/scripts/roots/chessGameRoot.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React from 'react'; // used by Webpack
+import React, {useState} from 'react'; // used by Webpack
 import {createRoot} from 'react-dom/client';
 import {SiteHeader} from '../components/common/SiteHeader';
 import {Chatbox} from '../components/common/Chatbox';
@@ -14,7 +14,18 @@ const titleAddition = configureTitleAddition(players);
 
 const use_chat = payload.preferences.use_chat;
 
+function ResignButton({resign}) {
+    const [doubleCheck, setDoubleCheck] = React.useState(false);
+    const resignDoubleCheck = () => {setDoubleCheck(true); setTimeout(() => setDoubleCheck(false), 2000);};
+    const resignConfirm = () => {resign(); sendResignation();}
+    const {color, innerText, clickBehavior} = doubleCheck ?
+        {color: 'red', innerText: 'Are you sure?', clickBehavior: resignConfirm} :
+        {color: 'black', innerText: 'Resign', clickBehavior: resignDoubleCheck};
+    return <button style={{color: color}} onClick={clickBehavior}>{innerText}</button>
+}
+
 function Page() {
+    const [gameEnded, setGameEnded] = useState(payload.game.completed);
     return <>
         <SiteHeader/>
         <main>
@@ -23,7 +34,7 @@ function Page() {
                 <Chessboard/>
                 <div id="belowChessboard">
                     <span id="chessStatus">Status: <span id="status"/></span>
-                    <button onClick={sendResignation}>Resign?</button>
+                    {!gameEnded && <ResignButton resign={() => setGameEnded(true)}/>}
                 </div>
             </div>
 
