5a4b766ad078b5fb9f498f071d92cf34ead19442
Eddie Lazar <eddie.lazar@yahoo.com>
2026-07-21 21:06:34 -0400

    #172 add logic for RCR and Power generation inside Power class, tidy up Powers list

--- src/backend/services/quad/Power.py
+++ src/backend/services/quad/Power.py
@@ -1,18 +1,17 @@
+import random
 
-# can define torus colors list here as well
+RCR_POWERS = ["Refurb","Destroy","Inhibit","Invert","Kamikaze","Acidic","Orb Spy","Parasite","Pilfer","Purify","Dredge","Spyware","Scramble","Swap","Tripwire","Trench","Wall","Recruit","Bankrupt","Learn","Teach"]
+# I believe "Wall" cannot be Radial
 
-# These need to be randomly assigned row, column, or radial if chosen
-RCR_POWERS = []
-
-ALL_POWERS = ["Multiply","refurb","Destroy","Inhibit","Invert","Kamikaze","Acid","Orb Spy","Parasite","Pilfer","Purify","Dredge","Spyware","Scramble","Swap","Tripwire","Relocate","Scavenger","Trench","Wall","Move Diagonal","Centerpult","Climb","2x","Grow Quadradius","Hotspot","Jump Proof","Invisible","Lower Tile","Move Again","Network Bridge","Bunker","Power Plant","Raise Tile","Recursive","Orbic Rehash","Snake Tunneling","Flat To Sphere","Recruit","Bombs","Bankrupt","Learn","Teach","Beneficiary","Moat","Plateau","Switcheroo","Smart Bombs"]
+ALL_POWERS = ["Multiply","Refurb","Destroy","Inhibit","Invert","Kamikaze","Acidic","Orb Spy","Parasite","Pilfer","Purify","Dredge","Spyware","Scramble","Swap","Tripwire","Relocate","Scavenger","Trench","Wall","Move Diagonal","Centerpult","Climb Tile","2x","Grow Quadradius","Hotspot","Jump Proof","Invisible","Lower Tile","Move Again","Network Bridge","Bunker","Power Plant","Raise Tile","Recursive","Orbic Rehash","Snake Tunneling","Flat To Sphere","Recruit","Bombs","Bankrupt","Learn","Teach","Beneficiary","Moat","Plateau","Switcheroo","Smart Bombs"]
 
 class Power:
 	name = None
 	rcr = None
 	count = 0
 
-	def __init__(self, name, rcr):
-		self.name = name
+	def __init__(self):
+		self.name = random.choice(ALL_POWERS)
 		self.count = 1
-		if name in RCR_POWERS:
-			self.rcr = rcr
+		if self.name in RCR_POWERS:
+			self.rcr = random.choice(["Row", "Column", "Radial"])
